40223b77330a0d918639f0a83c3d98e5c76eec60
Erik Verlage <everlage88@gmail.com>
2018-05-06 14:03:45 -0400

    Menu first pass

---
+++ Assets/Scripts/Menu/LevelButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+
+    public float baseScale = 0.2f;
+    public float hoverScale = 0.25f;
+
+    //public Image myTriangle;
+
+
+	void Start () {
+        setScaleXY(baseScale);
+
+        //myTriangle.alphaHitTestMinimumThreshold = 0.5f;
+	}
+
+	public void myOnMouseEnter()
+	{
+        setScaleXY(hoverScale);
+
+        Debug.Log("myOnMouseEnter");
+	}
+
+    public void myOnMouseExit()
+    {
+        setScaleXY(baseScale);
+        Debug.Log("myOnMouseExit");
+    }
+
+
+	public void setScaleXY(float newScale)
+    {
+        Vector3 scaleTemp = transform.localScale;
+        scaleTemp.x = newScale;
+        scaleTemp.y = newScale;
+        transform.localScale = scaleTemp;
+    }
+
+}
---
+++ Assets/menuButtonSelectable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuButtonSelectable : MonoBehaviour {
+
+	//   LevelButton parentLevelButtonScript;
+	//   float baseScale;
+	//   float hoverScale;
+
+	//void Start () {
+	//       parentLevelButtonScript = transform.parent.GetComponent<LevelButton>();
+	//       baseScale = parentLevelButtonScript.baseScale;
+	//       hoverScale = parentLevelButtonScript.hoverScale;
+	//}
+
+	void Start()
+	{
+        
+	}
+
+
+	public void test1()
+    {
+        Debug.Log("HELLO");
+
+    }
+   
+    public void test2()
+    {
+        Debug.Log("GOODBYE");
+    }
+
+}
