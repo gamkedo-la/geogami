1ceb12dbd8cb1a9c92162d1f434c5162b52e70f7
Erik Verlage <everlage88@gmail.com>
2018-05-06 14:30:27 -0400

    Menu updates

--- Assets/Scripts/Menu/MenuButton.cs
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -1,30 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LevelButton : MonoBehaviour {
+public class MenuButton : MonoBehaviour {
+	
 
-    public float baseScale = 0.2f;
-    public float hoverScale = 0.25f;
-    public string sceneToLoad;
+
+    public float baseScale = 1f;
+    public float hoverScale = 1.25f;
+
+    public string sceneToLoad = "Menu - Main";
 
     //public Image myTriangle;
 
 
-	void Start () {
+    void Start()
+    {
         setScaleXY(baseScale);
 
         //myTriangle.alphaHitTestMinimumThreshold = 0.5f;
-	}
+    }
 
-	public void myOnMouseEnter()
-	{
+    public void myOnMouseEnter()
+    {
         setScaleXY(hoverScale);
 
         Debug.Log("myOnMouseEnter");
-	}
+    }
 
     public void myOnMouseExit()
     {
@@ -32,19 +35,22 @@ public class LevelButton : MonoBehaviour {
         Debug.Log("myOnMouseExit");
     }
 
-
     public void myOnMouseClick()
     {
         SceneManager.LoadScene(sceneToLoad);
     }
 
 
-	public void setScaleXY(float newScale)
+    public void setScaleXY(float newScale)
     {
         Vector3 scaleTemp = transform.localScale;
         scaleTemp.x = newScale;
         scaleTemp.y = newScale;
         transform.localScale = scaleTemp;
     }
+
+
+
+
 
 }
